eebf7857d572dcff0905e257e42e145a4720410c
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2020-01-13 22:57:35 +0200

    Landing

---
+++ WEB Phone (Landing)/js/menu.js
@@ -0,0 +1,9 @@
+.menu li:nth-child(1) a {
+  animation: show 1s 1.1s ease 1 both;
+}
+.menu li:nth-child(2) a{
+  animation: show 0.2s 1.2s ease 1 both;
+}
+.menu li:nth-child(3) a{
+  animation: show 0.2s 1.3s ease 1 both;
+}
